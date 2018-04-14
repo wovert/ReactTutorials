b056c28f82ea2626dd11ffb2b608b5c423229e45
wovert <wovert@126.com>
2018-04-14 19:42:31 +0800

    npm run eject

--- liangjian-redux/src/App.js
+++ liangjian-redux/src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addGun, removeGun, addGunAsync } from './index.redux';
 
 class App extends Component {
   // constructor(props) {
   //   super(props)
   // }
   render() {
-    const store = this.props.store;
-    const num = store.getState();
-    const addGun = this.props.addGun;
-    const removeGun = this.props.removeGun;
-    const addGunAsync = this.props.addGunAsync;
+    //const store = this.props.store;
+    //const num = store.getState();
+    // const num = this.props.num;
+    // const addGun = this.props.addGun;
+    // const removeGun = this.props.removeGun;
+    // const addGunAsync = this.props.addGunAsync;
     return (
       <div>
-        <h1>现在有机枪{num}把</h1>
-        <button onClick={()=>store.dispatch(addGun())}>申请武器</button>
-        <button onClick={()=>store.dispatch(removeGun())}>上交武器</button>
-        <button onClick={()=>store.dispatch(addGunAsync())}>拖两天给武器</button>
+        <h1>现在有机枪{this.props.num}把</h1>
+        <button onClick={this.props.addGun}>申请武器</button>
+        <button onClick={this.props.removeGun}>上交武器</button>
+        <button onClick={this.props.addGunAsync}>拖两天给武器</button>
       </div>
     );
   }
 }
 
+const mapStatetoProps = (state) => {
+  return {num: state};
+}
+const actionCreators = {addGun, removeGun, addGunAsync};
+App = connect(mapStatetoProps, actionCreators)(App);
 export default App;
--- liangjian-redux/src/index.js
+++ liangjian-redux/src/index.js
@@ -3,20 +3,28 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
 import App from './App';
-import { counter, addGun, removeGun, addGunAsync } from './index.redux';
-
+// import { counter, addGun, removeGun, addGunAsync } from './index.redux';
+import { counter } from './index.redux';
 const reduxDevtools = window.devToolsExtension?window.devToolsExtension():f=>f;
 const store = createStore(counter, compose(
   applyMiddleware(thunk, logger),
   reduxDevtools
 ));
 
-function render() {
-  ReactDOM.render(<App store={store} addGun={addGun} removeGun={removeGun} addGunAsync={addGunAsync} />, document.getElementById('root'));
-}
-render();
+// function render() {
+//   ReactDOM.render(<App store={store} addGun={addGun} removeGun={removeGun} addGunAsync={addGunAsync} />, document.getElementById('root'));
+// }
+// render();
 
-// 状态变换时渲染
-store.subscribe(render);
+// // 状态变换时渲染
+// store.subscribe(render);
+
+ReactDOM.render(
+  (<Provider store={store}>
+    <App />
+  </Provider>),
+  document.getElementById('root')
+);
