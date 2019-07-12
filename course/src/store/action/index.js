744d2de84e849ea6c42c57f47a49c017581209b2
wovert <67668283@qq.com>
2019-07-12 17:18:54 +0800

    change

--- course/src/store/action/index.js
+++
@@ -1,8 +0,0 @@
-import course from './course';
-import person from './person';
-
-let action = {
-    course,
-    person
-};
-export default action;
--- course/src/store/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import course from './course';
-import person from './person';
-
-let reducer = combineReducers({
-    course,
-    person
-});
-export default reducer;
