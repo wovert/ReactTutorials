d0dfd1f190fd471a16c13418f9ce10e41762ccb0
wovert <67668283@qq.com>
2019-07-12 10:21:55 +0800

    change

--- react-mysql/manager/src/App.js
+++ react-mysql/manager/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 // yarn build 直接访问非根路由返回找不到页面
 // import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 // yarn build 只显示根路由(直接访问非根路由返回找不到页面)
 // 应用于 react-native
-import { MemoryRouter as Router, Route, Link } from "react-router-dom";
+// import { MemoryRouter as Router, Route, Link } from "react-router-dom";
 
 import { connect } from "react-redux";
 import View from "./pages/view";
@@ -34,6 +39,12 @@ class App extends Component {
           <Link className="btn btn-default glyphicon glyphicon-plus" to="/add">
             添加商品
           </Link>
+          <Redirect
+            className="btn btn-default glyphicon glyphicon-plus"
+            to="/add"
+          >
+            自动添加商品
+          </Redirect>
           <Route path="/" exact component={View} />
           <Route path="/add" component={Add} />
         </div>
