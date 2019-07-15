c2a18b6f9495899f4438cb34b6c6f4982394debe
wovert <67668283@qq.com>
2019-07-15 10:57:08 +0800

    custom swiper

---
+++ swiper/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Swiper from "./components/Swiper";
+
+// 公共样式
+import "../static/css/reset.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Swiper />
+      </header>
+    </div>
+  );
+}
+
+export default App;
