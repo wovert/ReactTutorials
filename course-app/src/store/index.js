3ffc9d8d30fbc2f22848ddc7e3538bd56111652c
wovert <67668283@qq.com>
2019-07-17 10:05:09 +0800

    计算REM“

--- course-app/src/store/index.js
+++ course-app/src/store/index.js
@@ -7,6 +7,7 @@ import reducer from "./reducer";
 let store = createStore(
   reducer,
   applyMiddleware(reduxLogger, reduxThunk, reduxPromise)
+  // window.devToolsExtension ? window.devToolsExtension() : f => f
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 export default store;
